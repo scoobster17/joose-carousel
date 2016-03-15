051abe234ad5f451956f40cfd032815eff8a65a5
Phil Gibbins <phil@philgibbins.com>
2016-03-15 22:26:44 +0000

    Added coverage for vertical carousels

--- js/joose-carousel.js
+++ js/joose-carousel.js
@@ -28,6 +28,7 @@ joose.carousel = (function(js) {
         if (!this.container) return false;
 
         // store the default page, set to first as default if not supplied (0-indexed)
+        var defaultPage = parseInt(this.container.getAttribute('data-default-page'));
         this.defaultPage = (typeof defaultPage === 'number' && defaultPage + '' !== 'NaN') ? defaultPage : 1;
 
         // store the pagination links
@@ -40,8 +41,8 @@ joose.carousel = (function(js) {
         // store other details
         this.pagesContainer = this.container.querySelector('#' + this.carouselId + ' > div');
         this.noOfPagesShown = this.container.getAttribute('data-pages-shown') || 1;
-
-        this.continuousScroll = typeof continuousScroll !== 'undefined' && continuousScroll === 'true' ? true : false;
+        this.horizontalCarousel = this.container.getAttribute('data-horizontal-carousel') === 'false' ? false : true;
+        this.continuousScroll = this.container.getAttribute('data-continuous-scroll') === 'false' ? false : true;
 
         // perform initial setup of the carousel
         this.init();
@@ -71,8 +72,11 @@ joose.carousel = (function(js) {
                 // 'scroll' to the selected page; add class to trigger's parent for styling flexibility
                 joose.utils.addClass(pageTrigger.parentNode, config.expandedClassForTrigger);
                 joose.utils.addClass(pageToShow, config.expandedClassForContent);
-                this.pagesContainer.style.marginLeft = '-' + ((pageNumber - 1) * this.pageWidth) + 'px';
-
+                if (this.horizontalCarousel) {
+                    this.pagesContainer.style.marginLeft = '-' + ((pageNumber - 1) * this.pageWidth) + 'px';
+                } else {
+                    this.pagesContainer.style.marginTop = '-' + ((pageNumber - 1) * this.pageHeight) + 'px';
+                }
             }
         },
 
@@ -131,13 +135,25 @@ joose.carousel = (function(js) {
                 this.container.setAttribute('data-component', 'carousel');
             };
 
-            // style the carousel (needs to be calculated dynamically)
-            this.pageWidth = (this.pagesContainer.clientWidth / this.noOfPagesShown);
-            this.pagesContainer.style.width = (this.pageWidth * this.noOfPages) + 'px';
+            // get the carousel pages
             this.pages = this.pagesContainer.querySelectorAll('[aria-labelledby]');
 
+            // style the carousel (needs to be calculated dynamically)
+            if (this.horizontalCarousel) {
+                this.pageWidth = (this.pagesContainer.clientWidth / this.noOfPagesShown);
+                this.pagesContainer.style.width = (this.pageWidth * this.noOfPages) + 'px';
+            } else {
+                this.pageHeight = parseInt(getComputedStyle(this.container).height) / this.noOfPagesShown; // no float therefore still in flow. Fixed height required on carousel
+                this.pagesContainer.style.height = (this.pageHeight * this.noOfPages) + 'px';
+            }
             for (var i=0; i<this.noOfPages; i++) {
-                this.pages[i].style.width = (this.pageWidth / this.noOfPagesShown) + 'px';
+                if (this.horizontalCarousel) {
+                    this.pages[i].style.width = (this.pageWidth / this.noOfPagesShown) + 'px';
+                } else {
+                    this.pages[i].style.height = (this.pageHeight / this.noOfPagesShown) + 'px';
+                }
+
+                // utilising existing loop for adding attr
                 this.pages[i].setAttribute('data-page-number', i+1);
             }
 
@@ -175,8 +191,6 @@ joose.carousel = (function(js) {
 
                 // get the carousel details
                 var carouselId = carousels[i].getAttribute('id');
-                var defaultPage = parseInt(carousels[i].getAttribute('data-default-page'));
-                var continuousScroll = carousels[i].getAttribute('data-continuous-scroll');
 
                 // default the id if none supplied
                 if (!carouselId) {
@@ -186,7 +200,7 @@ joose.carousel = (function(js) {
 
                 // record instance of a carousel to variable
                 if (!joose.carousels) joose.carousels = {};
-                joose.carousels[carouselId] = new Carousel(carouselId, defaultPage, continuousScroll);
+                joose.carousels[carouselId] = new Carousel(carouselId);
             }
         }
     };

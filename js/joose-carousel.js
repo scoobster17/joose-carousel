2be6edc8d3736e8f857d8f60a9917187194a3a6e
Phil Gibbins <phil@philgibbins.com>
2016-03-15 21:27:25 +0000

    Further fixes to functionality subject to tidy up

--- js/joose-carousel.js
+++ js/joose-carousel.js
@@ -18,7 +18,7 @@ joose.carousel = (function(js) {
     };
 
     // carousel constructor
-    var Carousel = function(carouselId, defaultPage) {
+    var Carousel = function(carouselId, defaultPage, continuousScroll) {
 
         // set the container element
         this.carouselId = carouselId;
@@ -28,7 +28,7 @@ joose.carousel = (function(js) {
         if (!this.container) return false;
 
         // store the default page, set to first as default if not supplied (0-indexed)
-        this.defaultPage = (typeof defaultPage !== 'undefined') ? defaultPage : 0;
+        this.defaultPage = (typeof defaultPage === 'number' && defaultPage + '' !== 'NaN') ? defaultPage : 1;
 
         // store the pagination links
         this.paginationLinks = document.querySelectorAll('#' + this.carouselId + ' > ul a');
@@ -38,6 +38,8 @@ joose.carousel = (function(js) {
         // store other details
         this.pagesContainer = this.container.querySelector('#' + this.carouselId + ' > div');
         this.noOfPagesShown = this.container.getAttribute('data-pages-shown') || 1;
+
+        this.continuousScroll = typeof continuousScroll !== 'undefined' && continuousScroll === 'true' ? true : false;
 
         // perform initial setup of the carousel
         this.init();
@@ -51,12 +53,18 @@ joose.carousel = (function(js) {
         showPage: function(pageId) {
             
             // get the page to open and it's trigger and page number
-            var pageTrigger = this.container.querySelector('[href="#' + pageId + '"]');
+            var pageTrigger = this.container.querySelector('[href="#' + pageId + '"]:not([data-direction])');
             var pageToShow = this.container.querySelector('#' + pageId);
             var pageNumber = pageToShow.getAttribute('data-page-number');
 
             // if the page exists
             if (pageToShow) {
+
+                // remove classes from other pages
+                for (var i=0; i<this.noOfPages; i++) {
+                    joose.utils.removeClass(this.paginationLinks[i+2].parentNode, config.expandedClassForTrigger);
+                    joose.utils.removeClass(this.pages[i], config.expandedClassForContent);
+                }
 
                 // 'scroll' to the selected page; add class to trigger's parent for styling flexibility
                 joose.utils.addClass(pageTrigger.parentNode, config.expandedClassForTrigger);
@@ -70,14 +78,48 @@ joose.carousel = (function(js) {
         bindEvents: function() {
             var carousel = this;
             
-            // skip the prev / next links
-            for (var i=2; i<this.noOfPaginationLinks; i++) {
-                this.paginationLinks[i].setAttribute('data-page-number', i+1);
+            for (var i=0; i<this.noOfPaginationLinks; i++) {
+                
+                // don't give a page number to a prev / next link
+                if (i>1) {
+                    this.paginationLinks[i].setAttribute('data-page-number', i-1);
+                } else {
+                    var direction = (i === 0) ? 'prev' : 'next';
+                    this.paginationLinks[i].setAttribute('data-direction', direction);
+                }
+
+                // add event to change page
                 this.paginationLinks[i].addEventListener('click', function(e) {
                     e.preventDefault();
                     carousel.showPage(this.getAttribute('aria-controls'));
+                    carousel.updatePrevNextPaginationLinks.call(carousel, parseInt(this.getAttribute('data-page-number')));
                 });
             }
+        },
+
+        // update the prev / next page pagination links details based on the page shown
+        updatePrevNextPaginationLinks: function(pageNo) {
+
+            var prevLink = this.paginationLinks[0];
+            var nextLink = this.paginationLinks[1];
+
+            var prevPageNumber = (pageNo-1 < 1) ? this.noOfPages : pageNo-1;
+            var nextPageNumber = (pageNo+1 > this.noOfPages) ? 1 : pageNo+1;
+
+            var prevPage = this.container.querySelector('section[data-page-number="' + prevPageNumber + '"]');
+            var nextPage = this.container.querySelector('section[data-page-number="' + nextPageNumber + '"]');
+
+            var prevPageId = prevPage.getAttribute('id');
+            var nextPageId = nextPage.getAttribute('id');
+
+            prevLink.setAttribute('data-page-number', prevPageNumber);
+            nextLink.setAttribute('data-page-number', nextPageNumber);
+
+            prevLink.setAttribute('href','#' + prevPageId);
+            nextLink.setAttribute('href','#' + nextPageId);
+
+            prevLink.setAttribute('aria-controls', prevPageId);
+            nextLink.setAttribute('aria-controls', nextPageId);
         },
 
         // initialise the carousel
@@ -91,12 +133,15 @@ joose.carousel = (function(js) {
             // style the carousel (needs to be calculated dynamically)
             this.pageWidth = (this.pagesContainer.clientWidth / this.noOfPagesShown);
             this.pagesContainer.style.width = (this.pageWidth * this.noOfPages) + 'px';
-            var pages = this.pagesContainer.querySelectorAll('[aria-labelledby]');
+            this.pages = this.pagesContainer.querySelectorAll('[aria-labelledby]');
 
             for (var i=0; i<this.noOfPages; i++) {
-                pages[i].style.width = (this.pageWidth / this.noOfPagesShown) + 'px';
-                pages[i].setAttribute('data-page-number', i+1);
+                this.pages[i].style.width = (this.pageWidth / this.noOfPagesShown) + 'px';
+                this.pages[i].setAttribute('data-page-number', i+1);
             }
+
+            // initialise the prev / next page link values
+            this.updatePrevNextPaginationLinks(this.defaultPage);
 
             // bind events to this instance of a carousel
             this.bindEvents();
@@ -127,6 +172,8 @@ joose.carousel = (function(js) {
 
                 // get the carousel details
                 var carouselId = carousels[i].getAttribute('id');
+                var defaultPage = parseInt(carousels[i].getAttribute('data-default-page'));
+                var continuousScroll = carousels[i].getAttribute('data-continuous-scroll');
 
                 // default the id if none supplied
                 if (!carouselId) {
@@ -136,7 +183,7 @@ joose.carousel = (function(js) {
 
                 // record instance of a carousel to variable
                 if (!joose.carousels) joose.carousels = {};
-                joose.carousels[carouselId] = new Carousel(carouselId);
+                joose.carousels[carouselId] = new Carousel(carouselId, defaultPage, continuousScroll);
             }
         }
     };
